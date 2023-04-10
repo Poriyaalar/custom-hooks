2d050be5a4fcc4a1e1217eb4a107f8196951e688
Karthirajan93 <106950862+Karthirajan93@users.noreply.github.com>
2023-04-10 19:16:46 +0530

    fix file

---
+++ modules/useLocalStorage.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+// ----------------------------------------------------------------------
+
+function useLocalStorage(key, defaultValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+
+    return storedValue === null ? defaultValue : JSON.parse(storedValue);
+  });
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.storageArea === localStorage && e.key === key) {
+        setValue(JSON.parse(e.newValue));
+      }
+    };
+    window.addEventListener("storage", listener);
+
+    return () => {
+      window.removeEventListener("storage", listener);
+    };
+  }, [key, defaultValue]);
+
+  const setValueInLocalStorage = (newValue) => {
+    setValue((currentValue) => {
+      const result =
+        typeof newValue === "function" ? newValue(currentValue) : newValue;
+
+      localStorage.setItem(key, JSON.stringify(result));
+
+      return result;
+    });
+  };
+
+  return [value, setValueInLocalStorage];
+}
+function useLocalStorageValue(key) {
+  const value = () => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue === null ? storedValue : JSON.parse(storedValue);
+  };
+  const getValue = value();
+  return getValue;
+}
+function useSetLocalStorage(key, defaultValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+
+    return storedValue === null ? defaultValue : JSON.parse(storedValue);
+  });
+  const setValueInLocalStorage = (newValue) => {
+    setValue((currentValue) => {
+      const result =
+        typeof newValue === "function" ? newValue(currentValue) : newValue;
+
+      localStorage.setItem(key, JSON.stringify(result));
+
+      return result;
+    });
+  };
+
+  return [setValueInLocalStorage];
+}
+export { useLocalStorage, useLocalStorageValue, useSetLocalStorage };
